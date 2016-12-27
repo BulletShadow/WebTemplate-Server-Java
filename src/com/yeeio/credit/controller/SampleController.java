10906155d33ab3f57aede52174381e37bf6e4013
sobinzeng <forever_honor@hotmail.com>
2016-12-27 15:06:48 +0800

    InitialCommit

---
+++ src/com/yeeio/credit/controller/SampleController.java
@@ -0,0 +1,18 @@
+package com.yeeio.credit.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = {"api/v1.0/sample"})
+public class SampleController {
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String addAppContentInfo(@PathVariable Long id) {
+
+        return id.toString();
+    }
+}  
